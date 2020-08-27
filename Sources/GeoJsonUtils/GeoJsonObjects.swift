305c67d9a171b5e4ee646a738f43c1514e79e948
Manuel Gomez <atcodingmanu@gmail.com>
2020-08-26 23:34:59 -0400

    Fixed bug where the whole array will fail decoding if one item failed

--- Sources/GeoJsonUtils/GeoJsonObjects.swift
+++ Sources/GeoJsonUtils/GeoJsonObjects.swift
@@ -120,7 +120,8 @@ public class GJFeatureCollection: Decodable {
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: GJFeatureCollectionCodingKeys.self)
         type = try container.decode(GJObjectType.self, forKey: .type)
-        features = try container.decode([GJFeature].self, forKey: .features)
+
+        features = try container.decode(FailableDecodableArray<GJFeature>.self, forKey: .features).elements
     }
 
 }
